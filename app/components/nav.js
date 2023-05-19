91ec0e4e31be19573381b3ce051c9dc8effb68b5
Jorwi García <jgjg.0503@gmail.com>
2023-05-18 21:54:08 -0400

    Firts push WEB Portafolio

---
+++ app/components/nav.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import navStyle from '../cssModules/nav.module.css'
+import { VscAccount } from "react-icons/vsc";
+import {Poppins} from 'next/font/google'
+const poppins=Poppins({
+  subsets:['latin'],
+  weight:'600'
+})
+
+export default function Nav() {
+  return (
+    <>
+      <nav className={poppins.className+' flex justify-between py-4 px-6'}>
+        <div className='flex items-center gap-2 text-3xl text-gray-300'>
+          <VscAccount/>
+          <h2 className='font-bold text-xl color_primary'>
+            JORWI GARCIA
+          </h2>
+        </div>
+        <div >
+          <ul className='flex gap-4 text-lg text-white'>
+            <li className={navStyle.active_link+' hoverColor'}>
+              Home
+            </li>
+            <li className={navStyle.hoverColor}>
+              Acerca de Mi
+            </li>
+            <li className={navStyle.hoverColor}>
+              Portafolio
+            </li>
+            <li className={navStyle.hoverColor}>
+              Contactame
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
