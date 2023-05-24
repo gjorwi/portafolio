0acdff68b74ca314bab3bce903088f1d51446a86
Jorwi García <jgjg.0503@gmail.com>
2023-05-24 16:02:48 -0400

    redeploy

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import myFonts from '@/utilidades/fuentes'
 export default async function Home () {
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-12 h-full relative z-0'>
+      <div className='grid grid-cols-1 md:grid-cols-12 h-full relative z-0 '>
         <div className='col md:col-span-7 z-10 w-full flex flex-col justify-center items-center md:items-start lg:items-end lg:pr-10'>
           <div className='lg:w-9/12 sm:w-full flex flex-col items-center md:items-start'>
             <h1 className={myFonts('bold').className+' text-3xl md:text-[2.2rem] text-center md:text-left'}>Hola, soy Jorwi García.</h1>
